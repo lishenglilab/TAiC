0e4f4cd568f5e9f444234e5d904233e4c25d0462
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-06-15 16:53:03 +0800

    Update 1.2. Calculation_of_transcript_specificity.R

--- Code/1.2. Calculation_of_transcript_specificity.R
+++ Code/1.2. Calculation_of_transcript_specificity.R
@@ -1,4 +1,5 @@
-# R version 
+# R version 4.0.2 (2020-06-22)
+# tidyverse version=1.3.1
 ## Calculate the expression specificity of transcripts across different cancer cell lineages or cancer types
 library(tidyverse)
 args=commandArgs(T)
