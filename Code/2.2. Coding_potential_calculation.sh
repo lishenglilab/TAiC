5268b5226da0dc6e872aadc51d4307ba0f2fbbac
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-06-15 16:39:15 +0800

    Update and rename 2. Coding_potential_calculation.sh to 2.2. Coding_potential_calculation.sh

--- Code/2.2. Coding_potential_calculation.sh
+++ Code/2.2. Coding_potential_calculation.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# gffread version=0.11.8
 # ORFfinder version=0.4.3
 # CPC2 version=1.0.1
 # CPAT version=3.0.4
 # HMMER version=3.3
+
+# gffread
+gffread -w CCLE_1017_merged_novel_noncoding.fa -g /home/public/reference/fa/human/GENCODE_v35_GRCh38.p13_genome.fa /home/weihu/RBP_transcript_drug_project/data/gtf/CCLE_1017_merged_novel_noncoding.gtf
 
 # ORFfinder
 /home/weihu/softwares/ORFfinder -ml 30 -strand plus -in /home/weihu/RBP_transcript_drug_project/data/fa/CCLE_1017_merged_novel_noncoding.fa -out novel_noncoding_transcript_ORFfinder > ORFfinder.log
