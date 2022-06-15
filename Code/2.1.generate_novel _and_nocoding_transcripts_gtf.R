0840580c342902ab8a1dc89e1c5745b628282bf7
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-06-15 16:36:33 +0800

    Create 2.1.generate_novel _and_nocoding_transcripts_gtf.R

---
+++ Code/2.1.generate_novel _and_nocoding_transcripts_gtf.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(rtracklayer)
+
+# import data
+ccle <- import("/home/weihu/RBP_transcript_drug_project/data/gtf/CCLE_1017_merged_filtered_0.1.gtf")
+genecode_v35 <- import("/home/public/reference/gtf/human/gencode.v35.annotation.gtf")
+
+# extract novel and noncoding transcripts
+noncoding_id <- unique(genecode_v35$transcript_id[genecode_v35$transcript_type != "protein_coding"])
+table(noncoding_id %in% ccle$transcript_id) # not all genecode v35 noncoding ids were found
+novel_id <- unique(grep("MSTRG", ccle$transcript_id, value = T))
+ccle_novel_noncoding <- ccle[ccle$transcript_id %in% c(noncoding_id, novel_id)]
+
+# export
+setwd("/home/weihu/RBP_transcript_drug_project/data/gtf")
+export(ccle_novel_noncoding, "CCLE_1017_merged_novel_noncoding.gtf", format = "gtf")
