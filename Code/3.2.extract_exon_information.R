e89079bea86ad673898bd9f566a5693202c7a84d
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-06-15 16:41:03 +0800

    Create 3.2.extract_exon_information.R

---
+++ Code/3.2.extract_exon_information.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(rtracklayer)
+
+# import data
+AnnoData <- import("/home/weihu/RBP_transcript_drug_project/data/gtf/CCLE_1017_merged_filtered_0.1.gtf")
+table(AnnoData$type) # check type
+
+# extract exon information
+index <- which(AnnoData$type == "exon") # extract indexes of exons
+ccle_exon <- data.frame(Type = AnnoData$type[index],
+                        Gene_Id = AnnoData$gene_id[index],
+                        Gene_Name = AnnoData$gene_name[index],
+                        Transcript_Id = AnnoData$transcript_id[index],
+                        AnnoData@seqnames[index],
+                        AnnoData@ranges[index],
+                        AnnoData@strand[index])
+colnames(ccle_exon)[5:9] <- c("Chr","Start","End","Width","Strand")
+
+# generate bed file
+ccle_exon_bed <- data.frame(ccle_exon$Chr, ccle_exon$Start, ccle_exon$End, paste0(ccle_exon$Transcript_Id, "_", 
+                                                                                  ccle_exon$Chr, ":",
+                                                                                  ccle_exon$Start, "|",
+                                                                                  ccle_exon$End, ":",
+                                                                                  ccle_exon$Strand),
+                            0, ccle_exon$Strand)
+
+
+# export
+setwd("/home/weihu/RBP_transcript_drug_project/data/bed")
+write.table(ccle_exon_bed, "CCLE_exon.bed", sep = "\t", row.names = F, col.names = F, quote = F)
