8145a9f9491ea4cd2332f5b0681dfea5bcfa3efb
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-06-20 09:31:56 +0800

    Update 4.3 Differentially expressed transcripts in 18 cancer types.R

--- Code/4.3 Differentially expressed transcripts in 18 cancer types.R
+++ Code/4.3 Differentially expressed transcripts in 18 cancer types.R
@@ -1,4 +1,4 @@
-## Perform differential expression analysis of transcripts in 18 cancer types of the TCGA cohort
+## Perform differential expression analysis of transcripts in 18 cancer types of the TCGA cohorts
 
 # R version 4.0.2 (2020-06-22)
 # tidyverse version=1.3.1
